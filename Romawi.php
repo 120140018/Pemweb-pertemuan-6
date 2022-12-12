5b3ae3ededdd87d0f6e504ab4bb2c2489f329194
120140018 <103373638+120140018@users.noreply.github.com>
2022-12-12 21:15:02 +0700

    Add files via upload

---
+++ Romawi.php
@@ -0,0 +1,36 @@
+<?php
+function intToRomawi($angka)  
+{ 
+    $n = intval($angka);
+    $hasil = ''; 
+ 
+    $list_angka = array(
+        'M'     => 1000, 
+        'CM'    => 900, 
+        'D'     => 500, 
+        'CD'    => 400, 
+        'C'     => 100, 
+        'XC'    => 90, 
+        'L'     => 50, 
+        'XL'    => 40, 
+        'X'     => 10, 
+        'IX'    => 9, 
+        'V'     => 5, 
+        'IV'    => 4, 
+        'I'     => 1
+    ); 
+    foreach ($list_angka as $romawi => $nilai)  
+    {
+        $cocok = intval($n / $nilai); 
+        $hasil .= str_repeat($romawi, $cocok); 
+        $n = $n % $nilai; 
+    } 
+    return $hasil; 
+} 
+
+// untuk mencetak LVIII
+echo intToRomawi(58);
+echo "\n\n";
+// untuk mencetak MCMXCIV
+echo intToRomawi(1994);
+?>
